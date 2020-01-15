d94b0f33f67f6d6a0582a31ce4a303363538e65a
Harriet Bicknell <harrietindubai@gmail.com>
2020-01-15 08:55:07 +0000

    Autoshop first commit

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,31 @@ class Mechanic
 
   attr_reader :name, :specialty
 
+  @@all = []
+
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
 end
+
+def self.all
+  @@all
+end
+
+def mechanics_cars
+  Car.all.select {|car| car.mechanic == self}
+end
+
+def car_owners
+  mechanics_cars.map {|car| car.car_owner}
+end
+
+def car_owners_names
+  car_owners.map {|owner| owner.name}
+end
+
+
 
 
 end
