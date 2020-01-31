650f8c2f337fa283a83ac1a01476f33fa05862f9
RikBala <rik280491@googlemail.com>
2020-01-31 10:51:48 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,30 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :car_owner, :mechanic
+  
+  @@all = [] 
 
-  def initialize(make, model, classification)
-    @make = make
-    @model = model
-    @classification = classification
+    def initialize(make, model, classification, car_owner, mechanic)
+      @make = make
+      @model = model
+      @classification = classification
+      @car_owner = car_owner 
+      @mechanic = mechanic 
+      @@all << self 
+    end
+
+    def self.all 
+      @@all 
+    end 
+
+    def car_classifications 
+      Car.all.map{ |car| car.classification }
+    end 
+
+    def mechanic_expertise 
+      Car.all.select{ |car| car.mechanic.specialty == self.classification }
+    end 
+
+
   end
 
-end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,29 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+ attr_reader :name, :specialty 
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+ @@all = [] 
 
+ def initialize(name, specialty)
+   @name = name
+   @specialty = specialty
+   @@all << self 
+ end
+
+ def self.all 
+   @@all 
+ end 
+
+ def serviced_cars
+    Car.all.select{|car| car.mechanic == self }
+ end 
+
+ def car_owner_mech
+  serviced_cars.map{|car| car.car_owner}
+ end 
+
+ def car_owner_mech_names
+  serviced_cars.map{|car| car.car_owner.name}
+ end 
 
 end
